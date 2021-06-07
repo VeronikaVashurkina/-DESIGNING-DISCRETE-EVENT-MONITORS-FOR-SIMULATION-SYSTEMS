202e4330a7d4ec5d0c630b260498a83b14bf1408
Вероника Вашуркина <67638959+VeronikaVashurkina@users.noreply.github.com>
2021-06-07 23:17:39 +0300

    Add files via upload

---
+++ Lab3MIS/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Lab3MIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(246, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 493);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "№";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Откуда";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Куда";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Прибыл";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Отправление";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(363, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(749, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 70);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Начало";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(38, 258);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(177, 354);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 187);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 641);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Расписание поездов";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab3MIS/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Lab3MIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //DataGridView dataGridView1 = new DataGridView();
+        private int clock = 1;
+        private bool stop = false;
+        Random rnd = new Random();
+        Random rand = new Random();
+        Random rnd1 = new Random();
+        
+        private int следующее_прибытие3;
+        private int num3;
+        private int следующее_прибытие4;
+        private int num4;
+        private int следующее_прибытие5;
+        private int num5;
+        private int следующее_прибытие6;
+        private int num6;
+        private int следующее_прибытие7;
+        private int num7;
+
+        public void Sobitie()
+        {
+            if (stop == false)
+            {
+                int number = rnd.Next(8);
+                int number1 = rnd1.Next(4);
+                switch (number1)
+                {
+                    case 0:
+                        {
+                            richTextBox1.Text = "Приехавший поезд, перегоняется на " + (number + 1) + " путь.";
+                            pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\421.png");
+                            break;
+
+                        }
+                    case 1:
+                        {
+                            richTextBox1.Text = "Из поезда вышло " + number + " пассажиров";
+                            pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\mov.png");
+                            break;
+
+                        }
+
+                    case 2:
+                        {
+                            richTextBox1.Text = "К товарному поезду прицепили еще " + (number + 1) + " вагонов";
+                            pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\woo.png");
+                            break;
+
+                        }
+
+                }
+            }
+        }
+        public void creatingEntry() {
+            stop = false;
+            double a =0.1 ;
+
+            
+            int T=100;
+
+            int n = 150;
+
+
+            int[] mass = new int[n];
+            double y = 0;
+            double xi = 0;
+
+
+            bool flag = false;
+        
+            
+            for (int j = 0; j < n; j++)
+            {
+                while ((flag != true) && (y <= T))
+                {
+
+                    xi = (-1.0 / a) * Math.Log(rand.NextDouble());
+
+                    if ((y + xi) > T)
+                    {
+                        flag = true;
+                    }
+
+                    else
+                    {
+                        mass[j]++;
+                        y += xi;
+                    }
+
+                    xi = 0;
+                }
+                flag = false;
+                y = 0;
+            }
+            var ilist = new List<int>();
+            ilist.AddRange(mass);
+            ilist.Sort();
+
+            
+
+            
+              int number=rnd.Next(8);
+
+            int sobitie = ilist[number];
+
+            int num = rnd.Next(10000);
+            //if (clock < 5)
+
+
+
+            if (sobitie == 3)
+            {
+                dataGridView1.Rows.Add(num, "Магнитогорск", "Сызрань", clock, clock + 1);
+                label2.Text = Convert.ToString(clock);
+                num3 = num;
+                следующее_прибытие3 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                clock++;
+                Sobitie();
+
+
+            }
+                if (sobitie == 4)
+                {
+                            dataGridView1.Rows.Add(num, "Самара", "Москва", clock, clock + 1);
+                            label2.Text = Convert.ToString(clock);
+                            num4 = num;
+                            следующее_прибытие4 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                            clock++;
+                    Sobitie();
+
+                }
+
+                if (sobitie == 5)
+                {
+                            dataGridView1.Rows.Add(num, "Тобольск", "Новокуйбышевск", clock, clock);
+                            label2.Text = Convert.ToString(clock);
+                            richTextBox1.Text = "Поезд, состоящий из " + (number + 1) + " вагонов, проехал без остановки т.к. был военный.";
+                            stop = true;
+                            pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\war.jpg");
+                            num5 = num;
+                            следующее_прибытие5 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                            clock++;
+                    Sobitie();
+
+                }
+
+                if (sobitie == 6)
+                {
+                            dataGridView1.Rows.Add(num, "Томск", "Калининград", clock, clock + 1);
+                            label2.Text = Convert.ToString(clock);
+                            num6 = num;
+                            следующее_прибытие6 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                            clock++;
+                    Sobitie();
+
+                }
+
+                if (sobitie == 7)
+                {
+
+                            label2.Text = Convert.ToString(clock);
+                            richTextBox1.Text = "Ремонтные работы на вокзале, в этот час все поезда отменены";
+                            stop = true;
+                            pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\401.png");
+                            num7 = num;
+                    Sobitie();
+                    clock++;
+
+                           
+                        }
+
+
+
+                
+               
+            
+            if (clock > 0)
+            {
+                if ((clock - 1) == следующее_прибытие3)
+                {
+                    dataGridView1.Rows.Add(num3, "Магнитогорск", "Сызрань", clock - 1, clock);
+                    label2.Text = Convert.ToString(clock);
+                    num3 = num;
+                    следующее_прибытие3 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                    Sobitie();
+                }
+                if ((clock - 1) == следующее_прибытие4)
+                {
+                    dataGridView1.Rows.Add(num4, "Самара", "Москва", clock - 1, clock);
+                    label2.Text = Convert.ToString(clock);
+                    num4 = num;
+                    следующее_прибытие4 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+
+                    Sobitie();
+                }
+                if ((clock - 1) == следующее_прибытие5)
+                {
+                    dataGridView1.Rows.Add(num5, "Тобольск", "Новокуйбышевск", clock - 1, clock);
+                    label2.Text = Convert.ToString(clock);
+                    richTextBox1.Text = "Поезд, состоящий из " + (number + 1) + " вагонов, проехал без остановки т.к. был военный.";
+                    stop = true;
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Вероника\\Downloads\\war.jpg");
+                    num5 = num;
+                    следующее_прибытие5 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+                    Sobitie();
+
+                }
+                if ((clock - 1) == следующее_прибытие6)
+                {
+                    dataGridView1.Rows.Add(num6, "Томск", "Калининград", clock - 1, clock);
+                    label2.Text = Convert.ToString(clock);
+                    num6 = num;
+                    следующее_прибытие6 = Convert.ToInt32(Math.Round(1 * (2 * rand.NextDouble() - 1) + 8, 6));
+
+                    Sobitie();
+                }
+                clock++;
+
+              
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            for (int i = 0; i <100; i++)
+            {
+                
+                creatingEntry();
+                _pause(2000);
+
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void _pause(int value)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < value)
+                Application.DoEvents();
+        }
+
+    }
+}
